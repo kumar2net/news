feee65c1dc1fa7994a34440aa97db9a4608db80e
kumar2net <kumar2net@gmail.com>
2025-11-06 17:43:17 +0530

    fix: api errors

--- client/src/pages/Blog.tsx
+++
@@ -1,5 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function Blog() {
-  return <Typography variant="h4">Blog Page</Typography>;
-}
--- client/src/pages/Me.tsx
+++
@@ -1,5 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function Me() {
-  return <Typography variant="h4">About Me</Typography>;
-}
